fa9d573c38279a076451cabfaf07602a7c31982a
lokachop <lokachop@gmail.com>
2021-12-25 18:37:27 +0100

    msg fixes

--- lua/autorun/client/cl_laastoolcatg.lua
+++ lua/autorun/client/cl_laastoolcatg.lua
@@ -1,5 +1,7 @@
 language.Add("tool.laas_objm.name", "LAAS Object Manipulator")
 language.Add("tool.laas_objm.desc", "Manipulate LAAS Entities!")
+language.Add("tool.laas_objm.0", "Press LMB to select an entity!")
+
 
 hook.Add("AddToolMenuCategories", "LAASAddToolMenuCategory", function()
 	spawnmenu.AddToolCategory("Main", "LAAS", "Lokachop's Advanced Anim System")
