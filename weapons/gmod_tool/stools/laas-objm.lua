7be911a196846dcb876c1033f85247c925bcc600
lokachop <lokachop@gmail.com>
2021-12-25 17:30:59 +0100

    object manipulator stool creation

---
+++ weapons/gmod_tool/stools/laas-objm.lua
@@ -0,0 +1,21 @@
+TOOL.Category = "LAAS"
+TOOL.Name = "LAAS Object Manipulator"
+
+TOOL.LeftClickAutomatic = true
+TOOL.RightClickAutomatic = true
+TOOL.RequiresTraceHit = true
+
+TOOL.Information = {
+	{ name = "left" },
+	{ name = "right" },
+	{ name = "reload" }
+}
+
+function TOOL:Reload(tr)
+
+end
+
+
+function TOOL:LeftClick(tr)
+
+end
