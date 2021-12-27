7aee0fe1b32246da3690f4b1e3bf640665a4f1ff
lokachop <lokachop@gmail.com>
2021-12-27 01:49:08 +0100

    added visuals although i still cant get deploy to work on singleplayer

--- lua/autorun/client/cl_laastoolcatg.lua
+++ lua/autorun/client/cl_laastoolcatg.lua
@@ -2,7 +2,7 @@ language.Add("tool.laas_objm.name", "LAAS Object Manipulator")
 language.Add("tool.laas_objm.desc", "Manipulate LAAS Entities!")
 language.Add("tool.laas_objm.0", "Press LMB to select an entity!")
 
-language.Add("tool.laas_rps.name", "LAAS Rail Path Selector")
+language.Add("tool.laas_rps.name", "LAAS Camera Path Selector")
 language.Add("tool.laas_rps.desc", "Make the camera path!")
 language.Add("tool.laas_rps.0", "Press LMB to add a point to the path!")
 
--- lua/weapons/gmod_tool/stools/laas_rps.lua
+++ lua/weapons/gmod_tool/stools/laas_rps.lua
@@ -9,12 +9,20 @@ TOOL.ClientConvar = {
 
 }
 
+if SERVER then
+	util.AddNetworkString("LokaLAASRPSLuaIsDumbWhyIsThisNotCalledOnTheClient")
+end
+
+
 function TOOL:Reload(tr)
 	print("re")
 end
 
 
 function TOOL:LeftClick(tr)
+	if game.SinglePlayer() then
+		
+	end
 	print("lc")
 end
 
@@ -27,11 +35,68 @@ function TOOL:Think()
 end
 
 function TOOL:Deploy()
-
+	if game.SinglePlayer() then
+		if SERVER then
+			net.Start("LokaLAASRPSLuaIsDumbWhyIsThisNotCalledOnTheClient")
+				net.WriteString("DeployCopy")
+			net.Send(Entity(1))
+		end
+	end
+	for k, v in pairs(self) do
+		print(k, v)
+	end
+	print("deployed")
 end
 
 function TOOL:Holster()
 
+end
+
+if CLIENT then
+	net.Receive("LokaLAASRPSLuaIsDumbWhyIsThisNotCalledOnTheClient", function()
+		local gettype = net.ReadString()
+	end)
+end
+
+
+
+local function DrawFilledCircle(xp, yp, s)
+	local circt = {}
+	local iterations = 16
+	for i = 0, iterations do
+		local curri = i / iterations
+		local xc = xp + math.sin(curri * (math.pi * 2)) * s
+		local yc = yp + math.cos(curri * (math.pi * 2)) * s
+
+		table.insert(circt, 0, {x = xc, y = yc})
+	end
+
+	surface.DrawPoly(circt)
+end
+
+function TOOL:DrawToolScreen(w, h)
+	draw.NoTexture()
+	surface.SetDrawColor(50, 50, 75)
+	surface.DrawRect(0, 0, w, h)
+
+	
+	local xc = CurTime() * 64 % (w + 64 + 32)
+	local yc = h * 0.9 + -math.abs(math.sin(CurTime() * 3.269) * h / 2)
+	
+	DrawFilledCircle((self.RenderToolScreenPrevXC or 0) - 32, self.RenderToolScreenPrevYC or 0, 32)
+
+	surface.SetDrawColor(100, 100, 150, 255)
+	DrawFilledCircle(xc - 32, yc, 32)
+	surface.SetDrawColor(115, 115, 150)
+	DrawFilledCircle((xc - 32) - 8, yc - 8, 16)
+
+
+	self.RenderToolScreenPrevXC = xc
+	self.RenderToolScreenPrevYC = yc
+
+
+	draw.SimpleText("LAAS v"..(tostring(LAAS.Version) or "error!"), "Trebuchet18", w, h, Color(255, 255, 255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_BOTTOM)
+	draw.SimpleText("#tool.laas_rps.name", "Trebuchet24", w / 2, 0, Color(255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP)
 end
 
 function TOOL.BuildCPanel(dform)
